4ae16391367a972c599d92eb44efe14274c2fb64
Ahmad Salama <cairo.innovation.team@gmail.com>
2019-01-08 15:53:00 +0200

    adding

--- Shared/PharmaServiceRoutes.cs
+++ Shared/PharmaServiceRoutes.cs
@@ -4,7 +4,7 @@ namespace ArVision.Service.Pharma.Shared
 {
     public static class PharmaServiceRoutes
     {
-        public const string ROUTE_PREFIX = @"Pharma";
+        public const string ROUTE_PREFIX = @"Pharma"; 
         public const string ROUTE_TEST = @"Test";
         public const string ROUTE_GET_VERSION = @"GetVersion";
         public const string ROUTE_GET_JUICE_LIST = @"GetJuiceList"; 
