8eb314c97b7a1dfa4ccb843fdd95106eb9415d1a
Ahmad Salama <cairo.innovation.team@gmail.com>
2019-01-08 15:52:08 +0200

    adding

--- Shared/PharmaServiceRoutes.cs
+++ Shared/PharmaServiceRoutes.cs
@@ -7,7 +7,7 @@ namespace ArVision.Service.Pharma.Shared
         public const string ROUTE_PREFIX = @"Pharma";
         public const string ROUTE_TEST = @"Test";
         public const string ROUTE_GET_VERSION = @"GetVersion";
-        public const string ROUTE_GET_JUICE_LIST = @"GetJuiceList";
+        public const string ROUTE_GET_JUICE_LIST = @"GetJuiceList"; 
         public const string ROUTE_GET_PATIENT_LIST = @"GetPatientList";
     }
 }
